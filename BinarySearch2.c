1ca53004d6b5d84f799206e2e602352eb79e2e7b
jeffrey.moon <novmoon73@gmail.com>
2018-09-29 10:12:17 +0900

    binary search

---
+++ BinarySearch2.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Score.h"
+
+int CompareScore(const void* _elem1, const void* _elem2)
+{
+  Score* elem1 = (Score*)_elem1;
+  Score* elem2 = (Score*)_elem2;
+
+  if(elem1->score > elem2->score) {
+    return 1;
+  } else if(elem1->score < elem2->score) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+int main(void)
+{
+  int Length = sizeof(DataSet) / sizeof(DataSet[0]);
+  int i = 0;
+  Score target;
+  Score* found = NULL;
+
+  qsort((void*)DataSet, Length, sizeof(Score), CompareScore);
+  target.number = 0;
+  target.score = 671.78;
+
+  found = bsearch((void*)&target, (void*)DataSet, Length, sizeof(Score), CompareScore);
+  printf("found: %d %f \n", found->number, found->score);
+  return 0;
+}
